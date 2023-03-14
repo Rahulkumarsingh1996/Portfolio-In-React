d2f780fb3c8efe8bf16f616349c8d227316c8f6a
Rahul kumar Singh <singhrahul841412@gmail.com>
2023-03-14 09:41:35 +0530

    first commit

---
+++ ScrollDown.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ScrollDown = () => {
+  return (
+   <div className="home__scroll">
+    <a href="#about" className="home__scroll-button button--flex">
+    <i class="uil uil-mouse-alt"></i>
+    <span className="home__scroll-name">Scroll Down</span><i class="uil uil-arrow-down home__scroll-arrow"></i>
+    </a>
+   </div>
+  )
+}
+
+export default ScrollDown
---
+++ Social.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="" className="home__social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/feed/?trk=sem-ga_campid.14650114788_asid.127961666300_crid.601257986827_kw.linkedin%20login_d.c_tid.kwd-12704335873_n.g_mt.e_geo.20455"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+
+      <a
+        href="https://web.whatsapp.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-whatsapp"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
