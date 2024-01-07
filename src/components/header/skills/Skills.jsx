07cdf0336fa6486495b5fbb596f20ca2d01c87f8
rahulcylsys841412 <118243747+Rahulkumarsingh1996@users.noreply.github.com>
2024-01-07 13:47:22 +0530

    Skill
    to work on skill part

--- src/components/header/skills/Skills.jsx
+++ src/components/header/skills/Skills.jsx
@@ -1,21 +1,122 @@
-import React from 'react';
+import React from "react";
 import "./skill.css";
-import Fronted from './Fronted';
-import Backend from './Backend';
-
+import Fronted from "./Fronted";
+import Backend from "./Backend";
+import AboutImg from "../../../assets/rahul.svg";
+import Html from "../../../assets/html.png";
+import Css from "../../../assets/css.png";
+import js from "../../../assets/javascript.png";
+import react from "../../../assets/React.png";
+import Node from "../../../assets/nodeJs.png";
+import MySql from "../../../assets/mysql1.png";
 const Skills = () => {
   return (
-   <section className="skills section" id="skills">
-    <h2 className="section__title">Skill</h2>
-    <span className="section__subtitle">My technical level</span>
+    <section className="skills section" id="skills">
+      <h2 className="section__title">Skill</h2>
+      <span className="section__subtitle">My technical level</span>
 
-    <div className="skills__container container grid">
-        <Fronted />
+      <div className="skills__container container grid">
+        {/* <Fronted />
 
-        <Backend />
-    </div>
-   </section>
-  )
-}
+        <Backend /> */}
+        <div className="skill_main_card">
+          <section>
+            <div class="container_card">
+              <div class="card">
+                <div class="content">
+                  <div class="imgBx">
+                    <img src={Html} />
+                  </div>
+                </div>
+                <ul class="sci">
+                  <li>
+                    <a href="">Html</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="card">
+                <div class="content">
+                  <div class="imgBx">
+                    <img src={Css} />
+                  </div>
+                </div>
+                <ul class="sci">
+                  <li>
+                    <a href="">Css</a>
+                  </li>
+                </ul>
+              </div>
 
-export default Skills
+              <div class="card">
+                <div class="content">
+                  <div class="imgBx">
+                    <img src={js} />
+                  </div>
+                </div>
+                <ul class="sci">
+                  <li>
+                    <a href="">JavaScript</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="card">
+                <div class="content">
+                  <div class="imgBx">
+                    <img src={react} className="react" />
+                  </div>
+                </div>
+                <ul class="sci">
+                  <li>
+                    <a href="">React</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="card">
+                <div class="content">
+                  <div class="imgBx">
+                    <img src={react} className="react" />
+                  </div>
+                </div>
+                <ul class="sci">
+                  <li>
+                    <a href="">React-Native</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="card">
+                <div class="content">
+                  <div class="imgBx">
+                    <img src={Node} />
+                  </div>
+                </div>
+                <ul class="sci">
+                  <li>
+                    <a href="">Node js</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="card">
+                <div class="content">
+                  <div class="imgBx">
+                    <img src={MySql} />
+                  </div>
+                </div>
+                <ul class="sci">
+                  <li>
+                    <a href="">My Sql</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
