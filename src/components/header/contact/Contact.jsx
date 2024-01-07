78914d448a72d46009be779e5b596eae1f397afb
rahulcylsys841412 <118243747+Rahulkumarsingh1996@users.noreply.github.com>
2024-01-08 01:10:31 +0530

    contact
    to add toastify in contact page

--- src/components/header/contact/Contact.jsx
+++ src/components/header/contact/Contact.jsx
@@ -1,32 +1,43 @@
 import React, { useRef, useState } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import "./contact.css";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Contact = () => {
   const form = useRef();
-  const[name , setName] = useState('');
-  const[email,setEmail] = useState('');
-  const[desc , setdesc] = useState('')
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [desc, setdesc] = useState("");
+  const notify = () => toast("Rahul will touch you soon!");
 
- const handlesubmit = (e) =>{
-if(name ==='' || email===''){
-  alert("Name and Email field are required..");
-}else{
-  e.preventDefault();
-  emailjs.sendForm('service_jrirb6f', 'template_ymy57me', form.current, 'T878eAAIFA063D1YP')
-      .then((result) => {
-          console.log("send to the email..",result.text);
-      }, (error) => {
-          console.log("Email error..",error.text);
-      });
-  console.log({ name, email, desc });
-  setName('');
-  setEmail('');
-  setdesc('');
-  console.log("Form submitted");
-  alert("Rahul will touch you soon");
-}
- }
+  const handlesubmit = (e) => {
+    if (name === "" || email === "") {
+      alert("Name and Email field are required..");
+    } else {
+      e.preventDefault();
+      emailjs
+        .sendForm(
+          "service_jrirb6f",
+          "template_ymy57me",
+          form.current,
+          "T878eAAIFA063D1YP"
+        )
+        .then(
+          (result) => {
+            console.log("send to the email..", result.text);
+            console.log({ name, email, desc });
+            setName("");
+            setEmail("");
+            setdesc("");
+            console.log("Form submitted");
+            notify()
+          },
+          (error) => {
+            console.log("Email error..", error.text);
+          }
+        );
+    }
+  };
   return (
     <section className="contact section" id="contact">
       <h2 className="section__title">Get in touch</h2>
@@ -69,7 +80,7 @@ if(name ==='' || email===''){
         <div className="contact__contact">
           <h3 className="contact__title">Write me your project</h3>
 
-          <form  className="contact__form" onSubmit={handlesubmit} ref={form}>
+          <form className="contact__form" onSubmit={handlesubmit} ref={form}>
             <div className="contact__form-div">
               <label className="contact__form-tag">Name</label>
               <input
@@ -77,7 +88,7 @@ if(name ==='' || email===''){
                 name="name"
                 className="contact__form-input"
                 placeholder="Insert your name"
-                onChange={(e)=>setName(e.target.value)}
+                onChange={(e) => setName(e.target.value)}
                 value={name}
               />
             </div>
@@ -88,7 +99,7 @@ if(name ==='' || email===''){
                 name="email"
                 className="contact__form-input"
                 placeholder="Insert your email"
-                onChange={(e)=>setEmail(e.target.value)}
+                onChange={(e) => setEmail(e.target.value)}
                 value={email}
               />
             </div>
@@ -100,17 +111,18 @@ if(name ==='' || email===''){
                 rows="10"
                 className="contact__form-input"
                 placeholder="Write your project"
-                onChange={(e) =>setdesc(e.target.value)}
+                onChange={(e) => setdesc(e.target.value)}
                 value={desc}
               ></textarea>
             </div>{" "}
             <br />
-            <button type="submit"  className="button button--flex" >
+            <button type="submit" className="button button--flex">
               send message<i class="uil uil-message"></i>
             </button>
           </form>
         </div>
       </div>
+      <ToastContainer />
     </section>
   );
 };
