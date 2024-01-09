9ea43f39b57edeae44e099dab3c47116018138c1
rahulcylsys841412 <118243747+Rahulkumarsingh1996@users.noreply.github.com>
2024-01-09 23:53:30 +0530

    App,header
    to add now bootstarp and loder before start the web

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import './App.css';
-import Header from './components/header/Header';
-import Home from './components/header/Home/Home';
-import About from './components/header/about/About';
-import Skills from './components/header/skills/Skills';
-import Qualification from './components/header/qualification/Qualification';
-import Contact from './components/header/contact/Contact';
-import Footer from './components/header/footer/Footer';
-import Scrollup from './components/header/scrollup/Scrollup';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/header/Home/Home";
+import About from "./components/header/about/About";
+import Skills from "./components/header/skills/Skills";
+import Qualification from "./components/header/qualification/Qualification";
+import Contact from "./components/header/contact/Contact";
+import Footer from "./components/header/footer/Footer";
+import Scrollup from "./components/header/scrollup/Scrollup";
+import DotLoader from "react-spinners/DotLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 10000);
+  }, []);
   return (
     <>
-    
-    <Header />
+      {loading ? (
+        <div className="app-loder">
+          <DotLoader
+            loading={loading}
+            // cssOverride={override}
+            size={100}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+            color="#e1b8f9"
+            speedMultiplier={1}
+          />
+        </div>
+      ) : (
+        <div>
+          <Header />
 
-    <main className='main'>
-    <Home />
-    <About />
-    <Skills />
-    <Qualification />
-    <Contact />
-    </main>
-    <Footer />
-    <Scrollup />
-   
-  </>
+          <main className="main">
+            <Home />
+            <About />
+            <Skills />
+            <Qualification />
+            <Contact />
+          </main>
+          <Footer />
+          <Scrollup />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,67 +1,111 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./header.css";
-
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faSun } from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
-/* ---------------- change background color -------------------- */
-    window.addEventListener("scroll", function () {
-        const header = this.document.querySelector(".header");
-        if (this.scrollY >= 80) header.classList.add("scroll-header");
-        else header.classList.remove("scroll-header");
-      });
+  /* ---------------- change background color -------------------- */
+  window.addEventListener("scroll", function () {
+    const header = this.document.querySelector(".header");
+    if (this.scrollY >= 80) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+  });
 
-    
-    const[Toggle, showMenu]= useState(false);
-    const [activeNav , setActiveNav] = useState("#home");
+  const [Toggle, showMenu] = useState(false);
+  const [activeNav, setActiveNav] = useState("#home");
+
+
 
   return (
     <header className='header'>
-        <nav className="nav container">
-            <a href="index.html" className='nav__logo'>Rahul</a>
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Rahul
+        </a>
 
-            <div className={Toggle ? "nav__menu show-menu" : 
-            "nav__menu"}>
-                <ul className="nav__list grid">
-                    <li className="nav__item">
-                        <a href="#home" onClick={()=> setActiveNav('#home')} className={activeNav === "#home" ? "nav__link active-link" : "nav__link"}>
-                            <i className="uil uil-estate nav__icon"></i>Home
-                        </a>
-                    </li>
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a
+                href="#home"
+                onClick={() => setActiveNav("#home")}
+                className={
+                  activeNav === "#home" ? "nav__link active-link" : "nav__link"
+                }
+              >
+                <i className="uil uil-estate nav__icon"></i>Home
+              </a>
+            </li>
 
-                    <li className="nav__item">
-                        <a href="#about"  onClick={()=> setActiveNav('#about')} className={activeNav === "#about" ? "nav__link active-link" : "nav__link"} >
-                            <i className="uil uil-user nav__icon"></i>About
-                        </a>
-                    </li>
+            <li className="nav__item">
+              <a
+                href="#about"
+                onClick={() => setActiveNav("#about")}
+                className={
+                  activeNav === "#about" ? "nav__link active-link" : "nav__link"
+                }
+              >
+                <i className="uil uil-user nav__icon"></i>About
+              </a>
+            </li>
 
-                    <li className="nav__item">
-                        <a href="#skills"  onClick={()=> setActiveNav('#skills')} className={activeNav === "#skills" ? "nav__link active-link" : "nav__link"}>
-                            <i className="uil uil-file-alt nav__icon"> </i>Skills
-                        </a>
-                    </li>
+            <li className="nav__item">
+              <a
+                href="#skills"
+                onClick={() => setActiveNav("#skills")}
+                className={
+                  activeNav === "#skills"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uil uil-file-alt nav__icon"> </i>Skills
+              </a>
+            </li>
 
+            <li className="nav__item">
+              <a
+                href="#qualification"
+                onClick={() => setActiveNav("#qualification")}
+                className={
+                  activeNav === "#qualification"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uil uil-scenery nav__icon"></i>Qualification
+              </a>
+            </li>
 
-                    
-                    <li className="nav__item">
-                        <a href="#qualification"  onClick={()=> setActiveNav('#qualification')} className={activeNav === "#qualification" ? "nav__link active-link" : "nav__link"}>
-                            <i className="uil uil-scenery nav__icon"></i>Qualification
-                        </a>
-                    </li>
+            <li className="nav__item">
+              <a
+                href="#contact"
+                onClick={() => setActiveNav("#contact")}
+                className={
+                  activeNav === "#contact"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uil uil-message nav__icon"></i>Contact
+              </a>
+            </li>
+          </ul>
+          <i
+            class="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
+        </div>
 
-                    <li className="nav__item">
-                        <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === "#contact" ? "nav__link active-link" : "nav__link"}>
-                            <i className="uil uil-message nav__icon"></i>Contact
-                        </a>
-                    </li>
-                </ul>
-                <i class="uil uil-times nav__close" onClick={()=> showMenu(!Toggle)}></i>
-            </div>
-
-            <div className="nav__toggle" onClick={()=> showMenu(!Toggle)}>
-            <i class="uil uil-apps"></i>
-            </div>
-        </nav>
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i class="uil uil-apps"></i>
+        </div>
+        
+        {/* <div className="toggle-color-button" onClick={onButtonClick}>
+          <FontAwesomeIcon icon={faSun} />
+        </div> */}
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
